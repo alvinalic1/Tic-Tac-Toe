e612419cbe5a1a0c86f90bfc783198a0ce14ed04
Alvin Alic <alvin.alic@gmail.com>
2024-10-10 17:56:46 -0500

    Add instance variable for player

---
+++ lib/Player.rb
@@ -0,0 +1,11 @@
+class Player
+  attr_accessor :name :character
+
+  def initialize(name, character)
+    self.name = name
+    self.character = character
+  end
+
+  def placement(row, column)
+  end
+end

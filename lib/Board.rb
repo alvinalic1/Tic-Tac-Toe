19842b6d2b589e886df639a501ea0fe21d02076d
Alvin Alic <alvin.alic@gmail.com>
2024-10-10 21:36:15 -0500

    Add method to check for winner diagnolly

--- lib/Board.rb
+++ lib/Board.rb
@@ -18,19 +18,18 @@ class Board
   end
 
   def check_winner?(character)
-    row1 = self.board.map {|row| row[0]}
-    row2 = self.board.map {|row| row[1]}
-    row3 = self.board.map {|row| row[2]}
-    if(row1.uniq == character)
-      return true
-    elsif(row2.uniq == character)
-      return true
-    elsif(row3.uniq == character)
-      return true
-    else 
-      return fale
-    end
-      
+      if(check_rows?(character) == true)
+        puts"Game over"
+        return true
+      elsif(check_columns?(character) == true)
+        puts "Game over"
+        return true
+      elsif(check_diagnols?(character) == true) 
+        puts "Game over"
+        return true
+      else 
+        return false
+      end
   end
 
   private
@@ -50,7 +49,7 @@ class Board
     end
   end
 
-  def check_columns?(board)
+  def check_columns?(character)
     column1 = self.board.transpose.map {|column| column[0]}
     column2 = self.board.transpose.map {|column| column[1]}
     column3 = self.board.transpose.map {|column| column[2]}
@@ -61,6 +60,16 @@ class Board
     elsif(column3.uniq == [character])
       return true
     else 
+      return false
+    end
+  end
+
+  def check_diagnols?(character)
+    if(self.board[0][0] == character && self.board[1][1] == character && self.board[2][2] == character)
+      return true
+    elsif(self.board[0][2] == character && self.board[1][1] == character && self.board[2][0] == character)
+      return true
+    else
       return false
     end
   end

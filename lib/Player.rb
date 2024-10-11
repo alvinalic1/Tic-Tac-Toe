c0cdc2d718620624d0809d357f39a8e12fdbe2ee
Alvin Alic <alvin.alic@gmail.com>
2024-10-10 21:43:55 -0500

    Delete unused method

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,11 +1,17 @@
+require_relative 'Board.rb'
+
+
 class Player
-  attr_accessor :name :character
+  attr_accessor :name, :character
 
   def initialize(name, character)
     self.name = name
     self.character = character
   end
 
-  def placement(row, column)
+  def place_character(row, column)
+    
   end
 end
+
+

306290947265d043ac1fdd91bb7857f687c60cb3
Alvin Alic <alvin.alic@gmail.com>
2024-10-11 19:13:00 -0500

    Add functions to get rid of repetitive code. Check for spot availability and make a method for player turns

--- lib/Game.rb
+++ lib/Game.rb
@@ -19,42 +19,73 @@ class Game
     winner = false
 
     until winner == true
-
-      if(@board.board.flatten.include?("_") == false)
-        puts "Cats Game, Game over"
-        break
-      end
       @board.print_board
+      player_turn(@player1, @board)
 
-      puts "#{@player1.name}'s turn"
-      puts "row number: "
-      row = gets.chomp
-      puts "column number: "
-      column = gets.chomp
-
-      @board.board[row.to_i][column.to_i] = @player1.character
-      @board.print_board
       if(@board.check_winner?(@player1.character))
         puts "Game over, #{@player1.name} wins"
         winner = true
         break
       end
 
-      puts "#{@player2.name}'s turn"
-      puts "row number: "
-      row = gets.chomp
-      puts "column number: "
-      column = gets.chomp
+      if(@board.board.flatten.include?("_") == false)
+        puts "Cats Game, Game over"
+        break
+      end
 
-      @board.board[row.to_i][column.to_i] = @player2.character
+      @board.print_board
+      player_turn(@player2, @board)
+     
       if(@board.check_winner?(@player2.character))
         puts "Game over, #{@player2.name} wins"
         winner = true
         break
       end
 
+      if(@board.board.flatten.include?("_") == false)
+        puts "Cats Game, Game over"
+        break
+      end
     end
   end
 
+  private
 
+  def spot_available?(board, row, column)
+    if @board.board[row][column] == "_"
+      return true
+    else
+      false
+    end
+  end
+
+  def player_turn(player, board)
+    complete = false
+    while complete != true
+      puts "#{player.name}'s turn"
+      puts "row number: "
+      row = gets.chomp
+      puts "column number: "
+      column = gets.chomp
+
+      if(spot_available?(board, row.to_i, column.to_i))
+        @board.board[row.to_i][column.to_i] = player.character
+        if(@board.check_winner?(player.character))
+          puts "Game over, #{player.name} wins"
+          break
+        end
+        complete = true
+      else
+        puts "That spot is unavailable, pick another"
+        next
+      end
+    end
+  end
+  
 end
+
+# if(@board.check_winner?(@player2.character))
+#   puts "Game over, #{@player2.name} wins"
+#   winner = true
+#   break
+# end
